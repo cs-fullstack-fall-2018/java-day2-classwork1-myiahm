3ca49bf4fbce336cf931fac6d88afcdb9d53b726
Myiah Moore <myiahsade@gmail.com>
2018-07-23 11:58:31 -0500

    check in

--- src/EX9.java
+++ src/EX9.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class EX9 {
     public static void main(String[] args) {
+        Scanner userPassword = new Scanner(System.in);
+        System.out.println("Enter password: ");
+        String password = userPassword.nextLine();
 
+        while (!password.equals("HardCorePassword")) {
+            System.out.println("Enter password: ");
+            System.out.println(password.equals("HardCorePassword"));
+            password = userPassword.nextLine();
+
+
+
+        } System.out.println("This is correct");{
+        }
     }
 }
