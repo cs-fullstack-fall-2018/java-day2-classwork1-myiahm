4ecbfc00a018a431d6502dcd013298173534cacc
Myiah Moore <myiahsade@gmail.com>
2018-07-23 13:17:08 -0500

    check in

---
+++ src/EX10.java
@@ -0,0 +1,14 @@
+import sun.plugin.dom.css.Counter;
+
+import java.util.Scanner;
+
+public class EX10 {
+    public static void main(String[] args) {
+        int counter = 0;
+
+           while (counter < 100) ;
+           System.out.println(counter);
+           counter += 1;{
+        }
+    }
+}
